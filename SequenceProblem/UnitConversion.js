980237748f00b6849ada02bfdafa35b7edcba41a
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-25 15:29:18 +0530

    ADDED: Unit Conversion

---
+++ SequenceProblem/UnitConversion.js
@@ -0,0 +1,10 @@
+let each_ft=12;
+let inch=42;
+let find_ft=inch/each_ft;
+console.log("Feet for 42 inch is "+find_ft);
+
+
+let length=40;
+let width=60;
+let one_Feet=0.3048;
+console.log("Area of rectangle is in Meter  "+ length*width*one_Feet);
