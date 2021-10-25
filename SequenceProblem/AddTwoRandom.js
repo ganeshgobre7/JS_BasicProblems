555ec41551d8b859161cdef96d04bbefb15dadbf
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-25 15:04:57 +0530

    ADDED:Addition of Two Random Number

---
+++ SequenceProblem/AddTwoRandom.js
@@ -0,0 +1,4 @@
+let random1=Math.floor(Math.random()*6)+1;
+let random2=Math.floor(Math.random()+6)+1;
+console.log("Dice Numbers Are : "+random1+" And "+random2);
+console.log("Addition is :",random1+random2);
