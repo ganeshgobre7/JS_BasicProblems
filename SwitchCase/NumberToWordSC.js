83be84e4c3602ad185f3492c1edfd18e82e2dbda
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-25 16:29:28 +0530

    ADDED: Number to Word using Switch Case

---
+++ SwitchCase/NumberToWordSC.js
@@ -0,0 +1,39 @@
+let number=Math.floor(Math.random()*10);
+console.log("The Number is :"+ number);
+
+    switch(number)
+{
+    case 0 : 
+        console.log("Zero");  
+        break;
+    case 1 : 
+        console.log("One");  
+        break;    
+    case 2 : 
+        console.log("Two"); 
+        break;    
+    case 3 : 
+        console.log("Three"); 
+        break;         
+    case 4 : 
+        console.log("Four"); 
+        break;       
+    case 5 : 
+        console.log("Five"); 
+        break;         
+    case 6 : 
+        console.log("Six");
+        break;    
+    case 7 : 
+        console.log("Seven");  
+        break;
+    case 8 : 
+        console.log("Eigth");
+        break;    
+    case 9 : 
+        console.log("Nine");  
+        break;                            
+    default:
+        console.log("Wrong Input"); 
+        break;             
+}
