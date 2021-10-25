e7ebf9b59c1e4a01220ab3380c22b0c0eca89a75
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-25 15:21:32 +0530

    ADDED: 5 Random 2 Digit value Addition and Average

---
+++ SequenceProblem/SumAvrg.js
@@ -0,0 +1,10 @@
+let Dice1=Math.floor(Math.random() * 89)+10;
+let Dice2=Math.floor(Math.random() * 89)+10;
+let Dice3=Math.floor(Math.random() * 89)+10;
+let Dice4=Math.floor(Math.random() * 89)+10;
+let Dice5=Math.floor(Math.random() * 89)+10;
+console.log("Two digit 5 random number is :",Dice1,Dice2,Dice3,Dice4,Dice5);
+let sum=Dice1+Dice2+Dice3+Dice4+Dice5;
+let average=sum/5;
+console.log("Sum of 5 random two digit numbers is "+sum);
+console.log("Average of 5 random two digit numbers is "+average);
